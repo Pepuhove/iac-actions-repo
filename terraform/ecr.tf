2d7a34365bf0ed2e8913cb1f2cb2dc5ae34fc825
Pepuhove <pepshove@gmail.com>
2026-01-07 19:37:16 +0200

    infra_on_AWS

---
+++ terraform/ecr.tf
@@ -0,0 +1,15 @@
+module "ecr" {
+  source  = "terraform-aws-modules/ecr/aws"
+  version = "3.1.0"
+
+  repository_name    = var.ecr_repo
+  registry_scan_type = "BASIC"
+  repository_type    = "private"
+
+  create_lifecycle_policy = false
+
+  tags = {
+    Terraform = "true"
+  }
+}
+# 
